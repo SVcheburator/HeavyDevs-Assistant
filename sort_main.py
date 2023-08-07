cb3a1ef3745f140101c6142748b6191b4b947c81
Cheburator <sashadanilenko06@gmail.com>
2023-08-07 20:16:14 +0300

    sort_main path input adited, main.py func logic added

--- sort_main.py
+++ sort_main.py
@@ -1,23 +1,21 @@
 import pathlib
-import sys
 from sort_functions import define_data, print_data, rm_empty_dirs, sort_data
 
 
-def main():
-    sysargv = sys.argv
-    if len(sysargv) > 1:
-        source = sysargv[1]        
+def sort_main_fucn(inp_path):
+    if len(inp_path) > 1:
+        source = inp_path       
     else:
         print("The path to folder should be passed as argument after script name")
         return
+    
     path = pathlib.Path(source)
+
     if not path.is_dir():
         print("The argument is path to file or folder doesn't exist")
         return
+    
     defined_files = define_data(path)
     sort_data(path, defined_files)
     rm_empty_dirs(path)
     print_data(defined_files)
-
-if __name__ == "__main__":
-    main()
