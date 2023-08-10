bbc965c837d542c94018b53c001ac130df9931d5
FTKV <mthkvv@gmail.com>
2023-08-10 13:18:00 +0100

    fix and rewrite search by tags

--- bot_assistant/notes_classes.py
+++ bot_assistant/notes_classes.py
@@ -63,6 +63,9 @@ class Note():
         self.flag_done = False
         self.date_modified = datetime.now()
 
+    def get_date_modified(self):
+        return self.date_modified
+
     def __str__(self):
         result = ""
         result += f"Title:\n{self.title}\n"
@@ -130,26 +133,16 @@ class Notes(UserDict):
     # Виконує пошук за тегами та показує сортований список нотаток.
     def search_and_sort_by_tags(self, tags):
         result_search_and_sort_body = []
-        result_note = []
         search_tags = []
         for tag in tags:
-            search_tags.append(f"#{tag.lower()}")
+            search_tags.append(f"#{tag.strip().lower()}")
         for note in self.data.values():
-            tags_in_note = []
-            note_tags = []
-            for tag_note in note.tags:
-                note_tags.append(tag_note)
+            note_tags = list(map(str, note.tags))
             for tag_search in search_tags:
                 if tag_search in note_tags:
-                    tags_in_note.append(tag_search)
-            if tags_in_note == search_tags:     # Нотатки я сортував по вмісту їхнього body.
-                result_search_and_sort_body.append(note.body)
-                result_note.append(note)
-        result_search_and_sort_body.sort(key=len)
-        for index, body in enumerate(result_search_and_sort_body):
-            for note in result_note:
-                if note.body == body:
-                    result_search_and_sort_body[index] = note
+                    result_search_and_sort_body.append(note)
+                    break
+        result_search_and_sort_body.sort(key=Note.get_date_modified, reverse=True)
         return result_search_and_sort_body
 
     def load_from_file(self, file):
